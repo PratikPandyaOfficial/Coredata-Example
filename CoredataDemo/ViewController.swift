a6bff8c394d0b2f98dc7fb5623d72eac3ba3086f
Pratik Pandya <pratikpandya@Pratiks-Mac-mini.local>
2019-01-10 17:08:23 +0530

    Initial Commit

---
+++ CoredataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoredataDemo
+//
+//  Created by Pratik Pandya on 10/01/19.
+//  Copyright © 2019 Pratik Pandya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
